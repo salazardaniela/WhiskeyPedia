2b08f04d7695419e56610a8fa83b854be44006da
Daniela Salazar <daniela.salazar@prodigious.com>
2015-07-10 07:11:42 -0500

    app previos

--- scripts/app/app.js
+++ scripts/app/app.js
@@ -4,89 +4,17 @@ define(
     [
         'jquery',
         'underscore',
-        'backbone',
-        'constantsWiki'
+        'backbone'
     ], 
     function(
         $,
         _,
-        Backbone,
-        constantsWiki
+        Backbone
     ) {
 
         'use strict';
 
-        // var ModelWiki = Backbone.Model.extend({
-        //     defaults: {
-        //         "text": 'some',
-        //         "href": '#'
-        //     }
-        // });
-
-        // var CollectionWiki = Backbone.Collection.extend({
-
-        //     model: ModelWiki,
-
-        //     url: "http://cocktails.wikia.com/api/v1/"
-
-        //     // "parse": function(data) {
-        //     //     return data.data.navData.navigation.wiki;
-        //     // }
-        // });
-
-        // var viewCocktail = Backbone.View.extend({
-
-        //     tagName: 'li',
-
-        //     "template": _.template($('#input-search').html()),
-
-        //     render: function(event) {
-        //         // $(this.el).html(this.template(this.model.attributes));
-        //         return this;
-        //     }
-        // });
-
-        // var ViewListWiki = Backbone.View.extend({
-
-        //     "el": "#cocktails-content",
-            
-        //     tagName: 'ul',
-
-        //     initialize: function(){
-        //         this.render();
-        //     },
-
-        //     render: function(event) {
-        //         _.each(this.model.models, function(cocktail) {
-        //             // console.log(cocktail);
-        //             var childView = new viewCocktail({"model": cocktail});
-        //             $(this.el).append(childView.render().el);
-
-        //         },this);
-
-        //         return this.el;
-        //     }
-        // });
-
-        // var viewWiki = Backbone.View.extend({
-
-        //     template: _.template($('#simple-template').html()),
-
-        //     render: function(event) {
-        //         $(this.el).html(this.template(this.model.toJSON()));
-        //         return this;
-        //     }
-        // }); 
-
-        // var wikeList = new CollectionWiki();
-
-        // wikeList.fetch({
-        //     "success": function() {       
-        //         var viewList = new ViewListWiki({"model": wikeList});
-        //         viewList.render();
-        //     }
-        // });
-
+        /* Start - connection when the user search */
         var ModelWiki = Backbone.Model.extend({
             defaults: {
                 "baseUrlSearch": "http://cocktails.wikia.com/api/v1/Search/List",
@@ -111,25 +39,80 @@ define(
                     batch = "&batch=" + baseData.batch,
                     namespaces = "&namespaces=" + baseData.namespaces;
 
-                console.log(keywords);
                 return baseUrl+keywords+limit+minArticleQuality+batch+namespaces
+            },
+
+            "parse": function(data) {
+                return data.items;
+            }
+        });
+
+        var ViewSingleWiki = Backbone.View.extend({
+            
+            "template": _.template($('#success-list').html()),
+
+            "events": {
+                "click .title": "showOverlay"
+            },
+
+            "render": function(event) {
+                $(this.el).html(this.template(this.model.attributes));
+                return this;
+            },
+
+            "showOverlay": function() {
+                var dataId = this.model.attributes.id;
+
+                var show = new ModelOverlay({
+                    "id": dataId
+                });
+
+                show.fetch({
+                    "success": function(data) {
+                        var overlay = new ViewOverlay({
+                            "model": show
+                        })
+                    }
+                });
             }
 
         });
 
         var ViewWiki = Backbone.View.extend({
-            "el": "#cocktails-content",
+
+            "el": "#cocktails-results",
 
             initialize: function() {
-                console.log('initialized');
+                this.render();
+            },
+
+            render: function() {
+                var self = this;
+                _.each(this.model.models, function(data) {
+                    var cocktails = new ViewSingleWiki({"model": data});
+                    $(self.el).append(cocktails.render().el);
+                })
             }
         });
 
-        var ModelSearch = Backbone.Model.extend({
-            defaults: {
-                "placeholder": "Search your Cocktail"
+        var ViewError = Backbone.View.extend({
+            
+            "el": "#cocktails-results",
+
+            "template": _.template($('#error-list').html()),
+
+            "initialize": function() {
+                this.render();
+            },
+
+            render: function() {
+                $(this.el).html(this.template(this.model.attributes));
             }
         });
+        /* End - connection when the user search */
+
+        /* Start - input to search */
+        var ModelSearch = Backbone.Model.extend();
 
         var ViewSearch = Backbone.View.extend({
             "el": "#search-wrapper",
@@ -153,6 +136,7 @@ define(
             processKey: function(e) {
                 if(e.which === 13) {
                     var keywords = $(e.currentTarget).children().val();
+                    $('#cocktails-results').empty();
                     this.submit(keywords);
                 }
             },
@@ -162,22 +146,150 @@ define(
                     "keywords": keywords
                 });
 
-                console.log(getSearch);
+                var errorMsj = new ModelSearch({
+                    "error": "Don't have any related with your search, Try with other word"
+                });
 
                 getSearch.fetch({
-                    "success": function() {
+                    "success": function(data) {
                         var searchRequest = new ViewWiki({
                             "model": getSearch
+                        })
+                    },
+                    "error": function() {
+                        var searchRequest = new ViewError({
+                            "model": errorMsj
                         })
                     }
                 });
             }
         });
 
-        var mainModelSearch = new ModelSearch({});
+        var mainModelSearch = new ModelSearch({
+            "placeholder": "Search your Cocktail"
+        });
 
         var mainSearch = new ViewSearch({
             "model": mainModelSearch
         })
+        /* End - input to search */
 
+        /* Start - populate data */
+        var ModelPopulate = Backbone.Model.extend();
+
+        var CollectionSearch = Backbone.Collection.extend({
+
+            model: ModelPopulate,
+
+            url: "http://cocktails.wikia.com/api/v1/Articles/Popular?expand=1&limit=3",
+
+            "parse": function(data) {
+                return data.items;
+            }
+
+        })
+
+        var ViewPopulate = Backbone.View.extend({
+
+            "template": _.template($('#each-popular').html()),
+
+            "events": {
+                "click .title": "showOverlay"
+            },
+
+            render: function(event) {
+                $(this.el).html(this.template(this.model.attributes));
+                return this;
+            },
+
+            "showOverlay": function() {
+                var dataId = this.model.attributes.id;
+
+                var show = new ModelOverlay({
+                    "id": dataId
+                });
+
+                show.fetch({
+                    "success": function(data) {
+                        var overlay = new ViewOverlay({
+                            "model": show
+                        })
+                    }
+                });
+            }
+
+        });
+
+        var ViewListPopulate = Backbone.View.extend({
+
+            "el": "#cocktails-content",
+
+            initialize: function() {
+                this.render();
+            },
+
+            render: function() {
+                var self = this;
+                _.each(this.model.models, function(populate) {
+                    var eachPopulate = new ViewPopulate({"model": populate});
+                    $(self.el).append(eachPopulate.render().el);
+                })
+            }
+
+        });
+
+        var mainModelPopulate = new CollectionSearch();
+
+        mainModelPopulate.fetch({
+            "success": function() {
+                var mainPopulate = new ViewListPopulate({
+                    "model": mainModelPopulate
+                });
+            }
+        })
+        /* End - populate data */
+
+        /* Start - Overlay */
+        var ModelOverlay = Backbone.Model.extend({
+            url: function() {
+                var baseUrl = "http://cocktails.wikia.com/api/v1/Articles/AsSimpleJson?",
+                    dataId = "id=" + this.attributes.id;
+
+                return baseUrl + dataId;
+            },
+
+            "parse": function(data) {
+                return data;
+            }
+        });
+
+        var ViewOverlay = Backbone.View.extend({
+            "el": "#overlayRegion",
+
+            "template": _.template($('#overlay').html()),
+
+            "events": {
+                "click .close": "closeOverlay",
+                "click .overlay-layer": "closeOverlay"
+            },
+
+            initialize: function(){
+                var number = this.model.attributes.id;
+                number = number.toString();
+                var data = this.model.attributes;
+                console.log(this);
+                this.render(data);
+            },
+
+            "render": function(data) {
+                console.log(data);
+                $(this.el).html(this.template(data));
+                return this;
+            },
+
+            "closeOverlay": function() {
+                $(this.el).empty();
+            }
+        });
+        /* End - Overlay */
     });
